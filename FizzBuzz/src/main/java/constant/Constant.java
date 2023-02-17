1397f92cc65848fe38e862b334f9897d3e50761d
yukishi24 <yukishi24github@gmail.com>
2023-02-17 23:13:35 +0900

    modify: Constant.java

--- FizzBuzz/src/main/java/constant/Constant.java
+++ FizzBuzz/src/main/java/constant/Constant.java
@@ -11,4 +11,7 @@ package constant;
  */
 public class Constant {
   public static final String headStr = "数値を入力してください。";
+  public static final String maxStr="最大値";
+  public static final String resultStrPrt="でFizzBuzzを行います。";
+  public static final String resultStr="結果";
 }
