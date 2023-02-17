88e3ab86019a3630c9d311e86a32d3943659ed60
yukishi24 <yukishi24github@gmail.com>
2023-02-17 22:59:58 +0900

    add: Main.java

---
+++ FizzBuzz/src/main/java/FizzBuzz/Main.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package FizzBuzz;
+
+/**
+ * 3の倍数の時にFizz<br>
+ * 5の倍数の時にBuzz<br>
+ * 15の倍数の時にFizzBuzzと表示
+ * 
+ * @author yukishi
+ *
+ */
+public class Main {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello World!!!");
+  }
+
+}
